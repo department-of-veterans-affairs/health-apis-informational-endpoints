e8ccbf140e9d4efd4fc008871ea5fd3281746bc2
Alec Bertossa <alec.bertossa@libertyits.com>
2019-10-30 15:06:41 -0500

    Fixed some things
    Well Known tests are passing now. The pretty method was a wrapper around a generic Jackson method so it's in its own place now. OpenAPI tests to come

--- base-r4/src/test/java/gov/va/api/health/informational/r4/capability/MetadataControllerTest.java
+++ base-r4/src/test/java/gov/va/api/health/informational/r4/capability/MetadataControllerTest.java
@@ -1,54 +1,52 @@
 package gov.va.api.health.informational.r4.capability;
 
+import static gov.va.api.health.informational.r4.utils.JsonUtils.prettyJson;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import gov.va.api.health.autoconfig.configuration.JacksonConfig;
 import gov.va.api.health.r4.api.resources.Capability;
-import lombok.SneakyThrows;
+import java.io.IOException;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.boot.test.context.ConfigFileApplicationContextInitializer;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @ContextConfiguration(
-  classes = {MetadataControllerTest.TestConfiguration.class, MetadataTestConfig.class},
-  initializers = ConfigFileApplicationContextInitializer.class
+  classes = {MetadataControllerTest.TestConfiguration.class, MetadataTestConfig.class}
 )
+@TestPropertySource("/application.properties")
 public class MetadataControllerTest {
 
   @Autowired CapabilityStatementProperties capabilityStatementProperties;
 
   @Autowired CapabilityResourcesProperties capabilityResourcesProperties;
 
-  @SneakyThrows
-  private String pretty(Capability capability) {
-    return JacksonConfig.createMapper()
-        .writerWithDefaultPrettyPrinter()
-        .writeValueAsString(capability);
-  }
-
   @Test
-  @SneakyThrows
   public void read() {
     MetadataController controller =
         new MetadataController(capabilityStatementProperties, capabilityResourcesProperties);
-    Capability old =
-        JacksonConfig.createMapper()
-            .readValue(getClass().getResourceAsStream("/capability.json"), Capability.class);
+    Capability old = null;
     try {
-      assertThat(pretty(controller.read())).isEqualTo(pretty(old));
+      old =
+          JacksonConfig.createMapper()
+              .readValue(getClass().getResourceAsStream("/capability.json"), Capability.class);
+    } catch (IOException e) {
+      System.out.println("Couldn't load capability.json into an object");
+      e.printStackTrace();
+    }
+    try {
+      assertThat(prettyJson(controller.read())).isEqualTo(prettyJson(old));
     } catch (AssertionError e) {
       System.out.println(e.getMessage());
       throw e;
     }
   }
 
-  @EnableAutoConfiguration
+  // Loads our properties file into a CapabilityStatementProperties bean that we can use
   @EnableConfigurationProperties(value = CapabilityStatementProperties.class)
   public static class TestConfiguration {}
 }
--- base-r4/src/test/java/gov/va/api/health/informational/r4/wellknown/WellKnownControllerTest.java
+++ base-r4/src/test/java/gov/va/api/health/informational/r4/wellknown/WellKnownControllerTest.java
@@ -1,73 +1,55 @@
 package gov.va.api.health.informational.r4.wellknown;
 
-// import static java.util.Arrays.asList;
-// import static org.assertj.core.api.Assertions.assertThat;
-//
-// import gov.va.api.health.autoconfig.configuration.JacksonConfig;
-// import gov.va.api.health.informational.r4.capability.CapabilityStatementProperties;
-// import gov.va.api.health.r4.api.information.WellKnown;
+import static gov.va.api.health.informational.r4.utils.JsonUtils.prettyJson;
+import static org.assertj.core.api.Assertions.assertThat;
 
+import gov.va.api.health.autoconfig.configuration.JacksonConfig;
+import gov.va.api.health.informational.r4.capability.CapabilityStatementProperties;
+import gov.va.api.health.r4.api.information.WellKnown;
+import java.io.IOException;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
+@ContextConfiguration(classes = {WellKnownControllerTest.TestConfiguration.class})
+@TestPropertySource("/application.properties")
 public class WellKnownControllerTest {
 
-  //  private WellKnown actual() {
-  //    return WellKnown.builder()
-  //        .tokenEndpoint("https://test.lighthouse.va.gov/token")
-  //        .authorizationEndpoint("https://test.lighthouse.va.gov/authorize")
-  //        .capabilities(
-  //            asList(
-  //                "context-standalone-patient",
-  //                "launch-standalone",
-  //                "permission-offline",
-  //                "permission-patient"))
-  //        .responseTypeSupported(asList("code", "refresh-token"))
-  //        .scopesSupported(asList("patient/Test.read", "launch/patient", "offline_access"))
-  //        .build();
-  //  }
-  //
-  //  private CapabilityStatementProperties conformanceProperties() {
-  //    return CapabilityStatementProperties.builder()
-  //        .security(
-  //            CapabilityStatementProperties.SecurityProperties.builder()
-  //                .authorizeEndpoint("https://test.lighthouse.va.gov/authorize")
-  //                .tokenEndpoint("https://test.lighthouse.va.gov/token")
-  //                .build())
-  //        .build();
-  //  }
-  //
-  //  @SneakyThrows
-  //  private String pretty(WellKnown wellKnown) {
-  //    return JacksonConfig.createMapper()
-  //        .writerWithDefaultPrettyPrinter()
-  //        .writeValueAsString(wellKnown);
-  //  }
-  //
-  //  @Test
-  //  @SneakyThrows
-  //  public void read() {
-  //    WellKnownController controller =
-  //        new WellKnownController(wellKnownProperties(), conformanceProperties());
-  //    try {
-  //      assertThat(pretty(controller.read())).isEqualTo(pretty(actual()));
-  //    } catch (AssertionError e) {
-  //      System.out.println(e.getMessage());
-  //      throw e;
-  //    }
-  //  }
-  //
-  //  private WellKnownProperties wellKnownProperties() {
-  //    return WellKnownProperties.builder()
-  //        .capabilities(
-  //            asList(
-  //                "context-standalone-patient",
-  //                "launch-standalone",
-  //                "permission-offline",
-  //                "permission-patient"))
-  //        .responseTypeSupported(asList("code", "refresh-token"))
-  //        .scopesSupported(asList("patient/Test.read", "launch/patient", "offline_access"))
-  //        .build();
-  //  }
+  @Autowired CapabilityStatementProperties capabilityStatementProperties;
+  @Autowired WellKnownProperties wellKnownProperties;
+
+  /**
+   * TODO: Making these tests more generic would be cool considering the only difference is class to
+   * interpret and type of controller
+   */
   @Test
-  public void t() {}
+  public void read() {
+    WellKnownController controller =
+        new WellKnownController(wellKnownProperties, capabilityStatementProperties);
+    WellKnown old = null;
+    try {
+      old =
+          JacksonConfig.createMapper()
+              .readValue(getClass().getResourceAsStream("/well-known.json"), WellKnown.class);
+    } catch (IOException e) {
+      System.out.println("Couldn't load well-known.json into an object");
+      e.printStackTrace();
+    }
+    try {
+      assertThat(prettyJson(controller.read())).isEqualTo(prettyJson(old));
+    } catch (AssertionError e) {
+      System.out.println(e.getMessage());
+      throw e;
+    }
+  }
+
+  @EnableConfigurationProperties(
+    value = {CapabilityStatementProperties.class, WellKnownProperties.class}
+  )
+  public static class TestConfiguration {}
 }
