65077b8fba1af657307df1885af93aba47ce6db5
Lesley Brown <lesley.brown@libertyits.com>
2019-12-06 17:19:05 -0500

    Remove superfluous source and add superfluous tests.

---
+++ base-dstu2/src/test/java/gov/va/api/health/informational/dstu2/conformance/ConformanceUtilitiesTest.java
@@ -0,0 +1,50 @@
+package gov.va.api.health.informational.dstu2.conformance;
+
+import static org.junit.Assert.assertEquals;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import gov.va.api.health.dstu2.api.resources.Conformance;
+import java.nio.file.Paths;
+import lombok.SneakyThrows;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.test.context.ConfigFileApplicationContextInitializer;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@ContextConfiguration(
+  classes = {ConformanceUtilitiesTest.TestConfiguration.class, MetadataTestConfig.class},
+  initializers = ConfigFileApplicationContextInitializer.class
+)
+public class ConformanceUtilitiesTest {
+
+  @Autowired ConformanceStatementProperties conformanceStatementProperties;
+
+  @Autowired ConformanceResourcesProperties conformanceResourcesProperties;
+
+  /** Object mapper to translate json to/from objects. */
+  private ObjectMapper mapper = new ObjectMapper();
+
+  /** Demonstrate the capability utilities can create a capability statement from properties. */
+  @Test
+  @SneakyThrows
+  public void capabilityUtilitiesTest() {
+
+    final Conformance expectedConformance =
+        mapper.readValue(
+            Paths.get("src", "test", "resources", "conformance.json").toFile(), Conformance.class);
+
+    final Conformance conformance =
+        ConformanceUtilities.initializeConformanceBuilder(
+            "Conformance", conformanceStatementProperties, conformanceResourcesProperties);
+
+    assertEquals(expectedConformance, conformance);
+  }
+
+  // Loads our properties file into a ConformanceStatementProperties bean that we can use.
+  @EnableConfigurationProperties(value = ConformanceStatementProperties.class)
+  public static class TestConfiguration {}
+}
