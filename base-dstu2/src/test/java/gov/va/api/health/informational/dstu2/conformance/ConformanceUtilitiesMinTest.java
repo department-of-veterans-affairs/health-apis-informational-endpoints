6e6d4f4cf0e2db00af580984a5eb48ff9d016409
Larry Crochet <larry.crochet@libertyits.com>
2020-06-25 13:59:51 -0400

    test items

--- base-dstu2/src/test/java/gov/va/api/health/informational/dstu2/conformance/ConformanceUtilitiesMinTest.java
+++ base-dstu2/src/test/java/gov/va/api/health/informational/dstu2/conformance/ConformanceUtilitiesMinTest.java
@@ -11,7 +11,6 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.test.context.ConfigFileApplicationContextInitializer;
-import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -31,7 +30,6 @@ public class ConformanceUtilitiesMinTest {
 
   @Test
   @SneakyThrows
-  @DirtiesContext
   public void capabilityUtilitiesTest() {
     conformanceStatementProperties.getSecurity().setManagementEndpoint(null);
     conformanceStatementProperties.getSecurity().setRevocationEndpoint(null);
