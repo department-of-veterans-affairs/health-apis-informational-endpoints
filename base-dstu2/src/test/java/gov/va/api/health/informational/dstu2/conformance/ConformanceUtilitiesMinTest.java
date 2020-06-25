b98c20f074bdec0ab6aa24825f7c2e1a51865a4f
Larry Crochet <larry.crochet@libertyits.com>
2020-06-25 12:50:43 -0400

    handle optional items

--- base-dstu2/src/test/java/gov/va/api/health/informational/dstu2/conformance/ConformanceUtilitiesMinTest.java
+++ base-dstu2/src/test/java/gov/va/api/health/informational/dstu2/conformance/ConformanceUtilitiesMinTest.java
@@ -6,19 +6,17 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import gov.va.api.health.dstu2.api.resources.Conformance;
 import java.nio.file.Paths;
 import lombok.SneakyThrows;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.test.context.ConfigFileApplicationContextInitializer;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.annotation.PropertySources;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @ContextConfiguration(
-  classes = {ConformanceUtilitiesMinTest.TestConfiguration.class, MetadataTestConfig.class},
+  classes = {ConformanceUtilitiesTest.TestConfiguration.class, MetadataTestConfig.class},
   initializers = ConfigFileApplicationContextInitializer.class
 )
 public class ConformanceUtilitiesMinTest {
@@ -30,25 +28,23 @@ public class ConformanceUtilitiesMinTest {
   /** Object mapper to translate json to/from objects. */
   private ObjectMapper mapper = new ObjectMapper();
 
-  /** Demonstrate the capability utilities can create a capability statement from properties. */
   @Test
   @SneakyThrows
   public void capabilityUtilitiesTest() {
-
     final Conformance expectedConformance =
         mapper.readValue(
             Paths.get("src", "test", "resources", "conformance_minimum.json").toFile(),
             Conformance.class);
-
     final Conformance conformance =
         ConformanceUtilities.initializeConformanceBuilder(
             "Conformance", conformanceStatementProperties, conformanceResourcesProperties);
-
     assertEquals(expectedConformance, conformance);
   }
 
-  // Loads our properties file into a ConformanceStatementProperties bean that we can use.
-  @EnableConfigurationProperties(value = ConformanceStatementProperties.class)
-  @PropertySources({@PropertySource("classpath:application_minimum.properties")})
-  public static class TestConfiguration {}
+  /** Demonstrate the capability utilities can create a capability statement from properties. */
+  @Before
+  public void clearoutOptional() {
+    conformanceStatementProperties.getSecurity().setManagementEndpoint(null);
+    conformanceStatementProperties.getSecurity().setRevocationEndpoint(null);
+  }
 }
